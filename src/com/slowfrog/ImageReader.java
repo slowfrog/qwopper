70eaeaea3ceea7f1780e0de8c0df9d23bbeb96fd
SlowFrog <laurentvaucher@gmail.com>
2011-02-13 22:48:21 +0100

    Beginning of code to read numbers in images. With unit test!

---
+++ src/com/slowfrog/ImageReader.java
@@ -0,0 +1,15 @@
+package com.slowfrog;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * That class is supposed to contain the code used to read the distance on
+ * screen. That's where all the image recognition fun lives. And it's unit
+ * tested too!
+ */
+public class ImageReader {
+
+  public static float readDistance(BufferedImage img) {
+    return 0;
+  }
+}
---
+++ test/com/slowfrog/TestImageReader.java
@@ -0,0 +1,21 @@
+package com.slowfrog;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
+
+public class TestImageReader {
+
+  @Test
+  public void readImages() throws IOException {
+    BufferedImage img = ImageIO.read(new File("img/dist_11.2.png"));
+    float result = ImageReader.readDistance(img);
+    assertThat(result, equalTo(11.2f));
+  }
+}
